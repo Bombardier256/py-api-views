b290564dbb9ef359ff8655c6d0e78835b9a0ff92
Bombardier256 <4477617@gmail.com>
2025-04-10 14:41:52 +0300

    Correct mistakes

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,11 +11,11 @@ from cinema.views import (
 )
 
 
-cinemahall_list = CinemaHallViewSet.as_view(actions={
+cinema_hall_list = CinemaHallViewSet.as_view(actions={
     "get": "list",
     "post": "create"
 })
-cinemahall_detail = CinemaHallViewSet.as_view(actions={
+cinema_hall_detail = CinemaHallViewSet.as_view(actions={
     "get": "retrieve",
     "put": "update",
     "patch": "partial_update",
@@ -31,9 +31,9 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_halls/", cinemahall_list, name="actor-list"),
+    path("cinema_halls/", cinema_hall_list, name="actor-list"),
     path(
-        "cinema_halls/<int:pk>/", cinemahall_detail, name="actor-detail"),
+        "cinema_halls/<int:pk>/", cinema_hall_detail, name="actor-detail"),
 ]
 
 app_name = "cinema"
